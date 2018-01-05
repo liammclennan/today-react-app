9f17db0e8d19c505efaaa7e828cb71e535e2377a
Liam McLennan <liam.mclennan@gmail.com>
2018-01-05 10:18:05 +1000

    react 4 - redux

--- src/index.js
+++ src/index.js
@@ -2,23 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import {createStore} from 'redux';
 
 function Today({ day, onNext }) {
     return <div>Today is {day.toString()} 
-		<button type="button" onClick={onNext}>Next</button></div>;
+        <button type="button" onClick={onNext}>Next</button></div>;
 }
-
-let day = new Date(2018,2,20);
 
 function onNext() {
-    day = new Date(day.getTime() + (24*3600*1000));
-    render();
+    store.dispatch({type: "NEXT_DAY"});
 }
 
-function render() {
-    ReactDOM.render(<Today day={day} onNext={onNext} />, document.getElementById('root'));
+function reducer(state = { day: new Date(2018,2,20) }, action) {
+    switch (action.type) {
+        case "NEXT_DAY":
+            const currentDay = store.getState().day;
+            return { day: new Date(currentDay.getTime() + (24*3600*1000))};
+        default: return state;
+    }
 }
-render();
+const store = createStore(reducer);
 
+store.subscribe(() => {
+    ReactDOM.render(<Today day={store.getState().day} onNext={onNext} />, 
+        document.getElementById('root'));
+});
+
+store.dispatch({type: "START"});
 
 registerServiceWorker();
