4f53d611e3ecaa3d5bcfe92e970e56fcf6981860
Liam McLennan <liam.mclennan@gmail.com>
2018-01-08 16:49:21 +1000

    react router

--- src/index.js
+++ src/index.js
@@ -4,10 +4,25 @@ import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux';
 import { connect, Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-function Today({ day, onNext }) {
-    return <div>Today is {day.toString()} 
-        <button type="button" onClick={onNext}>Next</button></div>;
+function Today(props) {
+    const day = new Date(props.match.params.datestring);
+    const nextDay = new Date(day.getTime() + (24*3600*1000));
+    const tomorrowDateString = `${nextDay.getFullYear()}-${nextDay.getMonth()+1}-${nextDay.getDate()}`;
+    return (
+        <div>Today is {day.toString()} 
+            <Link to={"/day/" + tomorrowDateString}>Next</Link>
+        </div>);
+}
+
+function Home() {
+    const randomBetween = (min, max) => Math.floor(Math.random() * (max-min+1) + min);
+    const randomDateString = `${randomBetween(1900, 2100)}-${randomBetween(1, 12)}-${randomBetween(1, 28)}`;
+
+    return <div><h1>Home</h1>
+        <Link to={`/day/${randomDateString}`}>{randomDateString}</Link>
+    </div>;
 }
 
 const ConnectedToday = connect(
@@ -33,7 +48,12 @@ const store = createStore(reducer);
 
 ReactDOM.render(
     <Provider store={store}>
-        <ConnectedToday />
+        <Router>
+            <div>
+            <Route exact path="/" component={Home} />
+            <Route path="/day/:datestring" component={ConnectedToday} />
+            </div>
+        </Router>
     </Provider>, 
     document.getElementById('root'));
 
